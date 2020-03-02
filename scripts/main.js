9213bba4cee38fbc4c28aaa0581b2b92e4e3419d
uehr <uehara.yudai@gmail.com>
2020-03-03 04:23:14 +0900

    fix: (スマホ)hoverアクションが戻らないバグ

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import * as futil from "./file-select.js"
 const splashBlurSec = 2
 const splashShowSec = 1
 const splashFadeoutSec = 1
+const hoverActionQuery = "#file-select-form,a,button"
 
 window.addEventListener("load", async () => {
     console.log("Preparing")
@@ -10,4 +11,24 @@ window.addEventListener("load", async () => {
     splash.finish(splashBlurSec, splashFadeoutSec)
     console.log("Ready")
     futil.setFileListener()
+
+    const isTouchDevice = (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
+
+    if (isTouchDevice) {
+        $(hoverActionQuery).on("touchstart", function () {
+            $(this).addClass("hover")
+        })
+        $(hoverActionQuery).on("touchend", function () {
+            $(this).removeClass("hover")
+        })
+    } else {
+        $(hoverActionQuery).hover(
+            function () {
+                $(this).addClass("hover")
+            },
+            function () {
+                $(this).removeClass("hover")
+            }
+        )
+    }
 }, false)
